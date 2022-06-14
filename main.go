dadacba6d4eb711518ce29e3428622465c35396a
Artyom Belski <artbelski@gmail.com>
2022-06-14 11:47:40 +0300

    Seventh try

--- main.go
+++ main.go
@@ -80,7 +80,7 @@ func (dl *dataList) fromJson(textJson string) {
 }
 
 func dataFromFile(filePath string) (dataList, error) {
-	file, err := os.OpenFile(filePath, os.O_RDONLY, 0644)
+	file, err := os.OpenFile(filePath, os.O_RDONLY|os.O_CREATE, 0644)
 	if err != nil { return nil, err }
 	byteContainment, err := ioutil.ReadAll(file)
 	if err != nil { return nil, err }
